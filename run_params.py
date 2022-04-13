74cd24b179acfbc816e46608a8e104b482684639
Tom Joshi-Cale <tj294@exeter.ac.uk>
2022-04-13 15:48:53 +0100

    update

--- run_params.py
+++ run_params.py
@@ -3,15 +3,15 @@ Parameters for rb_convect.py
 """
 import numpy as np
 
-a = 3
+a = 2
 Ny, Nz = 256, 64
-Pr = 1.0
-Ra = 1e6
+Pr = 1
+Ra = 1e4
 
 dt = 1e-5
 max_dt = 5e-4
 
-end_sim_time = 1.6
+end_sim_time = 1.5
 end_wall_time = np.inf
 end_iteration = np.inf
 
