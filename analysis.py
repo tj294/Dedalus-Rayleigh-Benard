dbf54175a354e966e9790150d681f550ad4be659
Tom Joshi-Cale <tj294@exeter.ac.uk>
2022-04-13 10:34:12 +0100

    Make y-z plane and update the Flux calculations

--- analysis.py
+++ analysis.py
@@ -41,24 +41,29 @@ args = parser.parse_args()
 
 direc = os.path.normpath(args.input) + "/"
 
+with h5py.File(direc + "run_params/run_params_s1.h5", "r") as f:
+    a = int(np.array(f["tasks"]["a"]))
+    Ra = float(np.array(f["tasks"]["Ra"]))
+    Pr = float(np.array(f["tasks"]["Pr"]))
 
-x = de.Fourier("x", 256, interval=(0, 3), dealias=3 / 2)
+
+y = de.Fourier("y", 256, interval=(0, a), dealias=3 / 2)
 z = de.Chebyshev("z", 64, interval=(0, 1), dealias=3 / 2)
-x = np.array(x.grid(1))
+y = np.array(y.grid(1))
 z = np.array(z.grid(1))
 # ====================
 # Plot Fluxes
 # ====================
-avg_t_start = 0.6
-avg_t_stop = 1.3
-
 if args.flux:
+
+    avg_t_start = float(input("Start average at: "))
+    avg_t_stop = float(input("End average at: "))
+
     with h5py.File(direc + "analysis/analysis_s1.h5", "r") as file:
         L_cond_arr = np.array(file["tasks"]["L_cond"])[:, 0]
         L_conv_arr = np.array(file["tasks"]["L_conv"])[:, 0]
+        KE = np.array(file["tasks"]["KE"])[:, 0]
         snap_t = np.array(file["scales"]["sim_time"])
-
-    print(L_cond_arr)
 
     if (
         (avg_t_start <= snap_t[0])
@@ -72,28 +77,38 @@ if args.flux:
             )
         )
         pass
-    ASI = (np.abs(snap_t - avg_t_stop)).argmin()
+    ASI = (np.abs(snap_t - avg_t_start)).argmin()
     if np.isnan(avg_t_stop):
         AEI = -1
     else:
         AEI = np.abs(snap_t - avg_t_stop).argmin()
     avg_t_range = snap_t[AEI] - snap_t[ASI]
+    print("Averaging between {} and {}".format(snap_t[ASI], snap_t[AEI]))
 
-    mean_L_cond = np.mean(np.array(L_cond_arr), axis=0)
-    mean_L_conv = np.mean(np.array(L_conv_arr), axis=0)
+    mean_L_cond = np.mean(np.array(L_cond_arr[ASI:AEI]), axis=0)
+    mean_L_conv = np.mean(np.array(L_conv_arr[ASI:AEI]), axis=0)
 
     mean_L_tot = mean_L_cond + mean_L_conv
     del_L = np.max(np.abs(1.0 - mean_L_tot))
     print("max del_L = {}".format(del_L))
 
     fig = plt.figure(figsize=(6, 6))
-    ax = fig.add_subplot(111)
-    ax.plot(z, mean_L_cond, "r", linestyle="-", label=r"$L_{cond}$")
-    ax.plot(z, mean_L_conv, "g", linestyle="-", label=r"$L_{conv}$")
-    ax.plot(z, mean_L_tot, "k", ls="-", label=r"$L_{total}$")
-    ax.set_xlabel("z")
-    ax.set_ylabel("L")
-    ax.legend()
+    fig.suptitle("2D\nRa={:.0e}, Pr={}".format(Ra, Pr))
+    KE_ax = fig.add_subplot(311)
+    KE_ax.plot(snap_t, KE, "k", label="Kinetic Energy")
+    KE_ax.set_xlabel(r"time [$\tau_\nu$]")
+    KE_ax.set_ylabel("KE")
+    KE_ax.axvspan(
+        snap_t[ASI], snap_t[AEI], color="r", alpha=0.5, label="Flux averaging"
+    )
+
+    L_ax = fig.add_subplot(212)
+    L_ax.plot(z, mean_L_cond, "r", linestyle="-", label=r"$L_{cond}$")
+    L_ax.plot(z, mean_L_conv, "g", linestyle="-", label=r"$L_{conv}$")
+    L_ax.plot(z, mean_L_tot, "k", ls="-", label=r"$L_{total}$")
+    L_ax.set_xlabel("z")
+    L_ax.set_ylabel("L")
+    L_ax.legend()
     plt.savefig(direc + "fluxes.png")
     plt.show()
     plt.close()
@@ -111,15 +126,15 @@ if args.heatmap:
         KE = np.array(file["tasks"]["KE"])[:, 0]
     with h5py.File(direc + "snapshots/snapshots_s1.h5", "r") as file:
         T = np.array(file["tasks"]["T"])
-        u = np.array(file["tasks"]["u"])
+        v = np.array(file["tasks"]["v"])
         w = np.array(file["tasks"]["w"])
         snap_t = np.array(file["scales"]["sim_time"])
         snap_iter = np.array(file["scales"]["iteration"])
 
-    xx, zz = np.meshgrid(x, z)
+    yy, zz = np.meshgrid(y, z)
 
     maxT = np.max(T)
-    maxU = np.max(u)
+    maxV = np.max(v)
     maxW = np.max(w)
 
     n_iter = len(T[:, 0:, 0])
@@ -131,49 +146,48 @@ if args.heatmap:
             fig = plt.figure(figsize=(8, 6))
             gs = gridspec.GridSpec(ncols=2, nrows=3, figure=fig)
             T_ax = fig.add_subplot(gs[0:2, 0])
-            u_ax = fig.add_subplot(gs[0, 1])
-            v_ax = fig.add_subplot(gs[1, 1])
+            v_ax = fig.add_subplot(gs[0, 1])
+            w_ax = fig.add_subplot(gs[1, 1])
             KE_ax = fig.add_subplot(gs[2, :])
             if (i % 50 == 0) and (i != 0):
                 sec_per_frame = (time.time() - start_time) / i
                 eta = sec_per_frame * (n_iter - i)
                 print(
-                    "image {}/{} at {:.3f}ips \t| ETA in {}m {}s".format(
+                    "image {}/{} at {:.3f}ips \t | ETA in {}m {}s".format(
                         i, n_iter, sec_per_frame, int(eta // 60), int(eta % 60),
-                    ),
-                    end="\r",
+                    )
                 )
-
             fig.suptitle(
-                "Iteration: {}\n".format(snap_iter[i])
-                + r"Sim Time: {:.2f} $\tau_\kappa$".format(snap_t[i])
+                "2D, Ra={:.0e}, Pr={}\n".format(Ra, Pr)
+                + "Iteration: {}\n".format(snap_iter[i])
+                + r"Sim Time: {:.2f} $\tau_\nu$".format(snap_t[i])
             )
-            c1 = u_ax.contourf(
-                xx,
+            c1 = v_ax.contourf(
+                yy,
                 zz,
-                np.transpose(u[i, :, :]),
-                levels=np.linspace(np.min(u), maxU),
+                np.transpose(v[i, :, :]),
+                levels=np.linspace(np.min(v), maxV),
                 cmap="coolwarm",
             )
-            c1_bar = fig.colorbar(c1, ax=u_ax)
-            c1_bar.set_label("u", rotation=0)
-            u_ax.set_ylabel("z")
-            u_ax.set_xlabel("x")
+            c1_bar = fig.colorbar(c1, ax=v_ax)
+            c1_bar.set_label("v", rotation=0)
+            v_ax.set_ylabel("z")
+            v_ax.set_xlabel("y")
 
-            c2 = v_ax.contourf(
-                xx,
+            c2 = w_ax.contourf(
+                yy,
                 zz,
                 np.transpose(w[i, :, :]),
                 levels=np.linspace(np.min(w), maxW),
                 cmap="coolwarm",
             )
-            c2_bar = fig.colorbar(c2, ax=v_ax)
+            c2_bar = fig.colorbar(c2, ax=w_ax)
             c2_bar.set_label("w", rotation=0)
-            v_ax.set_ylabel("z")
-            v_ax.set_xlabel("x")
+            w_ax.set_ylabel("z")
+            w_ax.set_xlabel("y")
 
             c3 = T_ax.contourf(
-                xx,
+                yy,
                 zz,
                 np.transpose(T[i, :, :]),
                 levels=np.linspace(0, maxT),
@@ -182,7 +196,7 @@ if args.heatmap:
             c3_bar = fig.colorbar(c3, ax=T_ax)
             c3_bar.set_label("T", rotation=0)
             T_ax.set_ylabel("z")
-            T_ax.set_xlabel("x")
+            T_ax.set_xlabel("Yy")
 
             KE_ax.plot(snap_t[:i], KE[:i], "k")
             KE_ax.set_xlabel(r"time [$\tau_\kappa$]")
@@ -214,6 +228,7 @@ if args.KE:
         KE = np.array(f["tasks"]["KE"])[:, 0]
         snap_t = np.array(f["scales"]["sim_time"])
     fig = plt.figure(figsize=(6, 4))
+    fig.suptitle("2D\nRa={:.0e}, Pr={}".format(Ra, Pr))
     ax = fig.add_subplot(111)
     ax.plot(snap_t, KE, "k")
     ax.set_xlabel(r"time [$\tau_\kappa$]")
